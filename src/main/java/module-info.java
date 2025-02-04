d009c0cb0da92afbc0bc9e7a9658fa264ef886f6
AnnRz <rzhavikina.anya@mail.ru>
2025-02-04 11:46:46 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.example.htmlreader {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires org.apache.commons.io;
+
+
+    opens org.example.htmlreader to javafx.fxml;
+    exports org.example.htmlreader;
+}
